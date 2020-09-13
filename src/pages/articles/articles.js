74c58bdc091e77012b087c4c0e1438532f5f28df
finick5210 <finick5210@yandex.ru>
2020-09-13 04:51:58 +0300

    Updated base urls for API

--- src/pages/articles/articles.js
+++ src/pages/articles/articles.js
@@ -6,7 +6,7 @@ import NewsApi from '../../js/api/NewsApi';
 const app = new App(
   document.querySelector('.root'),
   new Api({
-    baseUrl: 'https://api.news-explorer-yandex.tk',
+    baseUrl: 'https://news-explorer-yandex.tk/api',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
